633c922e183c339d4cd648983ada68fd3e32d7ea
yk0112 <yosikou0726@gmail.com>
2024-03-15 21:03:40 +0900

    feat: パース処理の追加

--- parser.go
+++ parser.go
@@ -3,6 +3,8 @@ package mini_sql
 import (
 	"errors"
 	"fmt"
+
+	"golang.org/x/text/currency"
 )
 
 func tokenFromKeyword(k Keyword) Token {
@@ -36,6 +38,89 @@ func helpMessage(tokens []*Token, cursor uint, msg string) {
 	}
 
 	fmt.Printf("[%d,%d]: %s, got: %s\n", c.Loc.Line, c.Loc.Col, msg, c.Value)
+}
+
+func parseToken(tokens []*Token, initialCursor uint, kind tokenKind) (*Token, uint, bool) {
+	cursor := initialCursor
+
+	if cursor >= uint(len(tokens)) {
+		return nil, initialCursor, false
+	}
+
+	current := tokens[cursor]
+
+	if current.Kind == kind {
+		return current, cursor + 1, true
+	}
+	return nil, initialCursor, false
+}
+
+func parseExpressions(tokens []*token, initialCursor uint, delimiter []token) (*[]*expression, uint, bool) {
+}
+
+func parseSelectStatement(tokens []*Token, initialCursor uint, delimiter Token) (*SelectStatement, uint, bool) {
+	cursor := initialCursor
+	if !expectToken(tokens, cursor, tokenFromKeyword(SelectKeyword)) {
+		return nil, initialCursor, false
+	}
+	cursor++
+
+	slct := SelectStatement{}
+
+	exps, newCursor, ok := parseExpressions(tokens, cursor, []Token{tokenFromKeyword(FromKeyword), delimiter})
+	if !ok {
+		return nil, initialCursor, false
+	}
+
+	slct.item = *exps
+	cursor = newCursor
+
+	if expectToken(tokens, cursor, tokenFromKeyword(FromKeyword)) {
+		cursor++
+		from, newCursor, ok := parseToken(tokens, cursor, IdentifierKind)
+		if !ok {
+			helpMessage(tokens, cursor, "Expected From token")
+			return nil, initialCursor, false
+		}
+
+		slct.from = *from
+		cursor = newCursor
+	}
+	return &slct, cursor, true
+}
+
+func parseStatement(tokens []*Token, initialCursor uint, delimiter Token) (*Statement, uint, bool) {
+	cursor := initialCursor
+
+	// SECRET statement
+	semicolonToken := tokenFromSymbol(SemicolonSymbol)
+	slct, newCursor, ok := parseSelectStatement(tokens, cursor, semicolonToken)
+	if ok {
+		return &Statement{
+			Kind:            SelectKind,
+			SelectStatement: slct,
+		}, newCursor, true
+	}
+
+	// INSERT statement
+	inst, newCursor, ok := parserInsertStatement(tokens, cursor, semicolonToken)
+	if ok {
+		return &Statement{
+			Kind:            InsertKind,
+			InsertStatement: inst,
+		}, newCursor, true
+	}
+
+	// CREATE statement
+	crtTbl, newCursor, ok := parserCreateTableStatement(tokens, cursor, semicolonToken)
+	if ok {
+		return &Statement{
+			Kind:                 CreateTableKind,
+			CreateTableStatement: crtTbl,
+		}, newCursor, true
+	}
+
+	return nil, initialCursor, false
 }
 
 func Parse(source string) (*Ast, error) {
